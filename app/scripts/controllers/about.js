15d1561c0e9ee474ba906be69d879b99cdea5f54
Denisa Profir <denisa.profir@linkscreens.com>
2017-10-24 20:52:35 +0300

    base code

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name refreshTokenExempleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the refreshTokenExempleApp
+ */
+angular.module('refreshTokenExempleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name refreshTokenExempleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the refreshTokenExempleApp
+ */
+angular.module('refreshTokenExempleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
